174ff0e5174eb3e9b5d82171f06f63c70ffde29f
Mahesh Kshirsagar <maksh@microsoft.com>
2021-02-26 18:06:13 +0000

    working example

--- maksh-playground/wsl-setup.sh
+++ maksh-playground/wsl-setup.sh
@@ -63,8 +63,7 @@ RESOURCEID_SUBNET_NODEPOOLS=$(az deployment group show -g rg-enterprise-networki
 az deployment group create -g rg-enterprise-networking-hubs -f networking/hub-regionA.json -p location=uksouth nodepoolSubnetResourceIds="['${RESOURCEID_SUBNET_NODEPOOLS}']"
 
 # Create the AKS cluster resource group.
-az group create --name maksh-bc2-aks-rg --location uksouth
-az group create --name maksh-bc3-aks-rg --location uksouth
+az group create --name maksh-pnp-aks-rg --location uksouth
 
 # Get the AKS cluster spoke VNet resource ID
 RESOURCEID_VNET_CLUSTERSPOKE=$(az deployment group show -g rg-enterprise-networking-spokes -n spoke-BU0001A0008 --query properties.outputs.clusterVnetResourceId.value -o tsv)
@@ -77,22 +76,22 @@ az deployment group create -g aks-rg -f ./cluster-stamp.json -p targetVnetResour
     aksIngressControllerCertificate=${AKS_INGRESS_CONTROLLER_CERTIFICATE_BASE64}
 
 # Deploy the cluster ARM template (Template File)
-az deployment group create -g maksh-bc3-aks-rg -f ./cluster-stamp.json -p "@azuredeploy.parameters.prod-maksh.json"
+az deployment group create -g maksh-pnp-aks-rg -f ./cluster-stamp.json -p "@azuredeploy.parameters.prod-maksh.json"
 
 # DEBUGGING ONLY: Delete a specific ARM template Deployment
 az deployment group delete --resource-group aks-rg --name cluster-stamp
 
 # Get the cluster name
-AKS_CLUSTER_NAME=$(az deployment group show -g maksh-bc2-aks-rg -n cluster-stamp --query properties.outputs.aksClusterName.value -o tsv)
+AKS_CLUSTER_NAME=$(az deployment group show -g maksh-pnp-aks-rg -n cluster-stamp --query properties.outputs.aksClusterName.value -o tsv)
 
 # Get AKS kubectl credentials.
-az aks get-credentials -g maksh-bc2-aks-rg -n $AKS_CLUSTER_NAME
+az aks get-credentials -g maksh-pnp-aks-rg -n $AKS_CLUSTER_NAME
 
 # Validate
 kubectl get nodes
 
 # Get your ACR cluster name
-ACR_NAME=$(az deployment group show -g  maksh-bc2-aks-rg -n cluster-stamp --query properties.outputs.containerRegistryName.value -o tsv)
+ACR_NAME=$(az deployment group show -g  maksh-pnp-aks-rg -n cluster-stamp --query properties.outputs.containerRegistryName.value -o tsv)
 
 # Import cluster management images hosted in public container registries
 az acr import --source docker.io/library/memcached:1.5.20 -n $ACR_NAME
@@ -107,3 +106,106 @@ kubectl create -f https://raw.githubusercontent.com/mspnp/aks-secure-baseline/ma
 
 # Wait for Flux to be ready
 kubectl wait -n cluster-baseline-settings --for=condition=ready pod --selector=app.kubernetes.io/name=flux --timeout=90s
+
+#Obtain the Azure Key Vault details
+KEYVAULT_NAME=$(az deployment group show --resource-group maksh-pnp-aks-rg -n cluster-stamp --query properties.outputs.keyVaultName.value -o tsv)
+
+# Give the current user permissions to import certificates.
+az keyvault set-policy --certificate-permissions import list get --upn $(az account show --query user.name -o tsv) -n $KEYVAULT_NAME
+
+# Import the AKS Ingress Controller's Wildcard Certificate
+cat traefik-ingress-internal-aks-ingress-contoso-com-tls.crt traefik-ingress-internal-aks-ingress-contoso-com-tls.key > traefik-ingress-internal-aks-ingress-contoso-com-tls.pem
+az keyvault certificate import -f traefik-ingress-internal-aks-ingress-contoso-com-tls.pem -n traefik-ingress-internal-aks-ingress-contoso-com-tls --vault-name $KEYVAULT_NAME
+
+# Remove Azure Key Vault import certificates permissions for current user
+az keyvault delete-policy --upn $(az account show --query user.name -o tsv) -n $KEYVAULT_NAME
+
+# Confirm policies are applied to the AKS cluster
+kubectl get constrainttemplate
+
+# Get the AKS Ingress Controller Managed Identity details
+export TRAEFIK_USER_ASSIGNED_IDENTITY_RESOURCE_ID=$(az deployment group show --resource-group maksh-pnp-aks-rg -n cluster-stamp --query properties.outputs.aksIngressControllerPodManagedIdentityResourceId.value -o tsv)
+export TRAEFIK_USER_ASSIGNED_IDENTITY_CLIENT_ID=$(az deployment group show --resource-group maksh-pnp-aks-rg -n cluster-stamp --query properties.outputs.aksIngressControllerPodManagedIdentityClientId.value -o tsv)
+
+# Ensure Flux has created the following namespace
+kubectl get ns a0008 -w
+
+# Create Traefik's Azure Managed Identity binding
+cat <<EOF | kubectl create -f -
+apiVersion: aadpodidentity.k8s.io/v1
+kind: AzureIdentity
+metadata:
+  name: podmi-ingress-controller-identity
+  namespace: a0008
+spec:
+  type: 0
+  resourceID: $TRAEFIK_USER_ASSIGNED_IDENTITY_RESOURCE_ID
+  clientID: $TRAEFIK_USER_ASSIGNED_IDENTITY_CLIENT_ID
+---
+apiVersion: aadpodidentity.k8s.io/v1
+kind: AzureIdentityBinding
+metadata:
+  name: podmi-ingress-controller-binding
+  namespace: a0008
+spec:
+  azureIdentity: podmi-ingress-controller-identity
+  selector: podmi-ingress-controller
+EOF
+
+# Create the Traefik's Secret Provider Class resource
+KEYVAULT_NAME=$(az deployment group show --resource-group maksh-pnp-aks-rg -n cluster-stamp --query properties.outputs.keyVaultName.value -o tsv)
+
+cat <<EOF | kubectl create -f -
+apiVersion: secrets-store.csi.x-k8s.io/v1alpha1
+kind: SecretProviderClass
+metadata:
+  name: aks-ingress-contoso-com-tls-secret-csi-akv
+  namespace: a0008
+spec:
+  provider: azure
+  parameters:
+    usePodIdentity: "true"
+    keyvaultName: $KEYVAULT_NAME
+    objects:  |
+      array:
+        - |
+          objectName: traefik-ingress-internal-aks-ingress-contoso-com-tls
+          objectAlias: tls.crt
+          objectType: cert
+        - |
+          objectName: traefik-ingress-internal-aks-ingress-contoso-com-tls
+          objectAlias: tls.key
+          objectType: secret
+    tenantId: $TENANTID_AZURERBAC
+EOF
+
+# Import the Traefik container image to your container registry
+az acr import --source docker.io/library/traefik:2.2.1 -n $ACR_NAME
+
+# Install the Traefik Ingress Controller
+kubectl create -f https://raw.githubusercontent.com/mspnp/aks-secure-baseline/main/workload/traefik.yaml
+
+# Wait for Traefik to be ready
+kubectl wait -n a0008 --for=condition=ready pod --selector=app.kubernetes.io/name=traefik-ingress-ilb --timeout=90s
+
+# Deploy the ASP.NET Core Docker sample web app
+kubectl create -f https://raw.githubusercontent.com/mspnp/aks-secure-baseline/main/workload/aspnetapp.yaml
+
+# Wait until is ready to process requests running
+kubectl wait -n a0008 --for=condition=ready pod --selector=app.kubernetes.io/name=aspnetapp --timeout=90s
+
+# Check your Ingress resource status
+kubectl get ingress aspnetapp-ingress -n a0008
+
+# Give it a try 
+kubectl run curl -n a0008 -i --tty --rm --image=mcr.microsoft.com/azure-cli --limits='cpu=200m,memory=128Mi'
+
+# From within the open shell
+curl -kI https://bu0001a0008-00.aks-ingress.contoso.com -w '%{remote_ip}\n'
+exit
+
+# Get Public IP of Application Gateway
+export APPGW_PUBLIC_IP=$(az deployment group show --resource-group rg-enterprise-networking-spokes -n spoke-BU0001A0008 --query properties.outputs.appGwPublicIpAddress.value -o tsv)
+
+# HOSTS File Validation
+#52.151.125.208 bicycle.contoso.com
